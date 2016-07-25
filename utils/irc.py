405190123eef17f43c506a3b404be6e6f086b8cd
Bradley Shaw <admin@indigotiger.me>
2016-07-25 09:55:24 +0000

    utils.irc: add List

--- utils/irc.py
+++ utils/irc.py
@@ -73,3 +73,45 @@ class Dict(dict):
         if isinstance(key, six.string_types):
             key = String(key)
         return key
+
+class List(list):
+
+    def __init__(self, *args, **kwargs):
+        super(List, self).__init__()
+        l = list(*args, **kwargs)
+        for item in l:
+            self.append(item)
+
+    def __contains__(self, item):
+        item = self.ircize_item(item)
+        return super(List, self).__contains__(item)
+
+    def __delitem__(self, item):
+        item = self.ircize_item(item)
+        return super(List, self).__delitem__(item)
+
+    def append(self, item):
+        item = self.ircize_item(item)
+        return super(List, self).append(item)
+
+    def insert(self, pos, item):
+        item = self.ircize_item(item)
+        return super(List, self).append(pos, item)
+
+    def remove(self, item):
+        item = self.ircize_item(item)
+        return super(List, self).remove(item)
+
+    def index(self, item):
+        item = self.ircize_item(item)
+        return super(List, self).index(item)
+
+    def count(self, item):
+        item = self.ircize_item(item)
+        return super(List, self).count(item)
+
+    @staticmethod
+    def ircize_item(item):
+        if isinstance(item, six.string_types):
+            item = String(item)
+        return item
