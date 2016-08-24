fe73b1f6fb2b6988aef9398b07dde002679951b3
Bradley Shaw <admin@indigotiger.me>
2016-08-24 22:23:10 +0100

    or not

--- eleos.py
+++ eleos.py
@@ -279,8 +279,11 @@ class Bot(object):
             cmd = "NOTICE"
         else:
             cmd = "PRIVMSG"
-        maxlen = 450 - len("{0} {1} :\r\n".format(cmd, target))
-        msgs = [msg[i:i+maxlen] for i in range(0, len(msg), maxlen)]
+        msg = str(msg).encode("UTF-8", "ignore")
+        maxlen = 400 - len("{0} {1} :\r\n".format(cmd, target).encode("UTF-8",
+            "ignore"))
+        msgs = [msg[i:i+maxlen].decode("UTF-8", "ignore")
+            for i in range(0, len(msg), maxlen)]
         for line in msgs:
             sendfunc("{0} {1} :{2}".format(cmd, target, line))
 
