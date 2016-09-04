9fcca374e39be9b025c7e7bd16ed4dc13b5ab930
Bradley Shaw <admin@indigotiger.me>
2016-09-04 15:49:12 +0100

    Add CTCP PING

--- handlers/CTCP.py
+++ handlers/CTCP.py
@@ -1,4 +1,5 @@
 import subprocess
+import time
 
 def on_CTCP(bot, event):
     ctcptype = event.arguments[0].upper()
@@ -10,3 +11,8 @@ def on_CTCP(bot, event):
     if ctcptype == "VERSION":
         version = subprocess.getoutput("git describe")
         bot.ctcpreply(event, ctcptype, "Eleos {0}".format(version))
+    elif ctcptype == "PING":
+        if len(args) > 0:
+            bot.ctcpreply(event, ctcptype, repr(" ".join(args))[1:-1])
+        else:
+            bot.ctcpreply(event, ctcptype, time.time())
