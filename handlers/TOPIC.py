3775b7960c6548e0afaf9d08726a55b275de8cbb
Bradley Shaw <admin@indigotiger.me>
2016-09-05 15:56:21 +0100

    Fix TOPIC handling

--- handlers/TOPIC.py
+++ handlers/TOPIC.py
@@ -8,9 +8,10 @@ def on_332(bot, event):
     bot.channels[channel]["topic"] = topic
 
 def on_TOPIC(bot, event):
-    channel = event.arguments[0]
-    topic = event.arguments[1]
-    if len(topic) > 0:
-        bot.channels[channel]["topic"] = topic
-    else:
-        bot.channels[channel]["topic"] = None
+    channel = event.target
+    topic = event.arguments[0]
+    if channel in bot.channels:
+        if len(topic) > 0:
+            bot.channels[channel]["topic"] = topic
+        else:
+            bot.channels[channel]["topic"] = None
