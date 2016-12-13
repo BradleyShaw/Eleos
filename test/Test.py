34a2071f60a6de001795408a4874e89b823dae4c
Bradley Shaw <indigotiger@sigint.pw>
2016-12-13 16:15:31 +0000

    Configure GitLab CI

---
+++ test/Test.py
@@ -0,0 +1,16 @@
+import signal
+import os
+
+import utils.plugins as plugins
+import utils.hook as hook
+import utils.task as task
+
+class Test(plugins.Plugin):
+
+    @hook.event(type="JOIN")
+    def on_join(self, bot, event):
+        # Allow 10 seconds for something to fuck up
+        # then exit cleanly
+        test_task = task.run_in(10, os.kill, (os.getpid(), signal.SIGINT))
+
+Class = Test
