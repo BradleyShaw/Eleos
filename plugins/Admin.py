99f73e7441bc78345b75f535dad23bb9cec93b17
Bradley Shaw <bs@sigint.pw>
2017-05-20 14:03:19 +0100

    Add part command [ci skip]

--- plugins/Admin.py
+++ plugins/Admin.py
@@ -76,14 +76,45 @@ class Admin(plugins.Plugin):
     def joincmd(self, bot, event, args):
         """<channel> [<key>]
 
-        Makes the bot attempt to join <channel>
-        (using <key> if specified).
+        Makes the bot attempt to join <channel> (using <key> if specified).
         """
         args = args.split(" ", 1)
         if len(args) == 1:
             bot.join(args[0])
         else:
             bot.join(args[0], args[1])
+
+    @hook.command(flags="a", global_only=True)
+    def part(self, bot, event, args):
+        """[<channel>] [<message>]
+
+        Makes the bot part <channel> (with <message> is specified). <channel> is
+        only required if the command isn't sent in the channel itself.
+        """
+        try:
+            split_args = self.space_split(args)
+            if event.target == bot.nick:
+                channel = split_args[0]
+                if len(split_args) > 1:
+                    message = args.lstrip(" ").split(" ", 1)[1]
+            elif len(split_args) > 0:
+                if bot.is_channel(split_args[0]):
+                    channel = split_args[0]
+                    if len(split_args) > 1:
+                        message = args.lstrip(" ").split(" ", 1)[1]
+                else:
+                    channel = event.target
+                    message = args
+            else:
+                channel = event.target
+                message = None
+        except IndexError:
+            bot.reply(event, self.get_help("part"))
+        else:
+            if channel not in bot.channels:
+                bot.reply(event, "Error: I'm not in {0}.".format(channel))
+                return
+            bot.part(channel, message)
 
     @hook.command(flags="a", global_only=True)
     def raw(self, bot, event, args):
